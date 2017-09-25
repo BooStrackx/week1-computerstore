9bf92d0f01f061454e30650834151ea963b34035
BooStrackx <boo808@hotmail.com>
2017-09-25 11:01:32 +0200

    Oplossing Taak 2

---
+++ src/be/pxl/computerstore/hardware/Processor.java
@@ -0,0 +1,25 @@
+package be.pxl.computerstore.hardware;
+
+public class Processor extends ComputerComponent {
+
+	private double clockspeed;
+
+	public Processor(String vendor, String name, double price, double clockspeed) {
+		super(vendor, name, price);
+		setClockspeed(clockspeed);
+	}
+
+	public double getClockspeed() {
+		return clockspeed;
+	}
+
+	public void setClockspeed(double clockspeed) {
+
+		if (clockspeed < 0.7) {
+			this.clockspeed = 0.7;
+		} else {
+			this.clockspeed = clockspeed;
+		}
+	}
+
+}
